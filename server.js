76d92106eca7c5e31042760e810db868228a28f9
Walter Vetrivel <waltervetrivel@yahoo.in>
2019-09-14 18:18:36 +1000

    Added test routes

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ const express = require('express');
 
 const connectDB = require('./config/db');
 
-const PORT = process.env.PORT || 3000;
-
 const app = express();
+// Connect database
 connectDB();
 
-app.get('/', (req, res) => res.end('API Working'));
+// Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
-app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`);
-});
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
