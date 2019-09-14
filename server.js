340488e7f1c1434fe7e27db4446277592e378897
Walter Vetrivel <waltervetrivel@yahoo.in>
2019-09-14 18:11:56 +1000

    Added database connection

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
-const app = express();
+
+const connectDB = require('./config/db');
 
 const PORT = process.env.PORT || 3000;
+
+const app = express();
+connectDB();
 
 app.get('/', (req, res) => res.end('API Working'));
 
