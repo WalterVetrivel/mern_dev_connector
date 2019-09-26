a79682f37c6b1189c3af60941da359e2eea5815e
Walter Vetrivel <waltervetrivel@yahoo.in>
2019-09-26 22:50:40 +1000

    Added basic routes and markup for Login and Register

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,27 @@
 import React, {Fragment} from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 import './App.css';
 
 const App = () => {
 	return (
-		<Fragment>
-			<Navbar />
-			<Landing />
-		</Fragment>
+		<Router>
+			<Fragment>
+				<Navbar />
+				<Route exact path="/" component={Landing} />
+				<section className="container">
+					<Switch>
+						<Route exact path="/login" component={Login} />
+						<Route exact path="/register" component={Register} />
+					</Switch>
+				</section>
+			</Fragment>
+		</Router>
 	);
 };
 
